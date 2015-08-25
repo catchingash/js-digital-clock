6d98322cbd9324211c30acabea9dd5b571e5ba34
Ashley Watkins <AWatkins31@gmail.com>
2015-08-25 14:19:18 -0700

    Adds local date and time to the view :)

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function displayTime() {
+  var t = new Date();
+  var clock = getPadded(t.getHours()) + ':' + getPadded(t.getMinutes()) + ':' + getPadded(t.getSeconds());
+
+  var clockDiv = document.getElementById('clock');
+
+  clockDiv.innerText = clock;
+}
+
+function getPadded(n) {
+  var str = n.toString();
+  while(str.length < 2) {
+    str = '0' + str;
+  }
+  return str;
+}
+
+function displayDate() {
+  var t = new Date();
+  var date = t.getMonth() + '-' + t.getDay() + '-' + t.getFullYear();
+
+  var clockDiv = document.getElementById('date');
+
+  clockDiv.innerText = date;
+}
+
+window.setInterval(displayTime, 100);
+window.setInterval(displayDate, 100);
